7b6983fa447e21e4d4ac7a5d4490abe6836a9281
MAHI2363 <punnatimaheswari@gmail.com>
2017-12-11 12:37:17 +0530

    imgchnages

--- app.js
+++ app.js
@@ -31,3 +31,32 @@ myapp.controller('MyCtrl',function($scope){
         console.log(student);
     };
 });
+
+
+myapp.controller('MyCtrlImg',function($scope){
+    $scope.imgList= [
+    'https://www.hdwallpapers.in/thumbs/2017/bond_4k-t2.jpg',
+    'https://www.hdwallpapers.in/thumbs/2016/cute_haircut_baby_girl-t2.jpg',
+    'https://1.bp.blogspot.com/-i5ED1KQeuSE/WiEA7mv_a0I/AAAAAAAAQLo/stcqbxxxfPM5Gsqp2MfodjRy-rq0WD1nACLcBGAs/s400/max-resolution-hdpictures-of-fairbaby.jpg'
+   ];
+
+   $scope.index = 0;
+   $scope.increaseIndex = function(){
+       if($scope.index ===  $scope.imgList.length-1)
+       {
+        $scope.index = 0;
+       }else{
+         $scope.index =  $scope.index+1;
+       }
+   };
+
+   $scope.decreaseIndex = function(){
+    if($scope.index ===  0)
+    {
+        $scope.index =  $scope.imgList.length -1;        
+    }else{
+        $scope.index =  $scope.index-1;
+    }
+   };
+
+});
