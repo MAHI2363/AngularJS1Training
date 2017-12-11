b1758d776e54b537dbdbce3eb69daf9affc834fa
MAHI2363 <punnatimaheswari@gmail.com>
2017-12-11 11:53:29 +0530

    inital comment

--- app.js
+++ app.js
@@ -5,5 +5,29 @@ myapp.controller('MyCtrl',function($scope){
 
     $scope.sayHi=function(){
         alert('Hi');
+        $scope.title = "modified from controller";
     }
+
+    $scope.students = [
+        {
+            name: 'maheswari',
+            department: 'Arts',
+            dob: '28-06-1987'
+        },
+        {
+            name: 'Naveen',
+            department: 'commers',
+            dob: '28-06-1980'
+        },
+        {
+            name: 'arya',
+            department: 'Maths',
+            dob: '28-06-1976'
+        }
+    ];
+     
+    $scope.onStundentSelect = function(student){
+        $scope.selectedStudent = student;
+        console.log(student);
+    };
 });
